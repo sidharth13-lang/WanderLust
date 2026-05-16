e85bfc6d865abd4721b59f1e14eb73fb44cf841c
sidharth13-lang <sidharthr365@gmail.com>
2026-05-17 02:20:30 +0530

    Fixed MongoDB Atlas connection and init DB

--- init/index.js
+++ init/index.js
@@ -1,26 +1,32 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("connected to DB");
 }).catch((err) => {
     console.log(err);
 });
-async function main(){
+
+async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-const initDB = async() =>{
+const initDB = async () => {
    await Listing.deleteMany({});
-   initData.data = initData.data.map((obj) => ({...obj,owner:"687f841c5e144f5b2f244bbd"}));
+   initData.data = initData.data.map((obj) => ({
+      ...obj,
+      owner: "687f841c5e144f5b2f244bbd"
+   }));
    await Listing.insertMany(initData.data);
    console.log("data was initialized");
 };
 
 initDB();
-
-
